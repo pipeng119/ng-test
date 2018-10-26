cc0d51091335a18d376b18dc9525ed9714d56924
pipeng119 <15640117732>
2018-10-26 10:39:53 +0800

    ng router finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-test';
+  title = '鞠躬学习，死而后已';
 }
---
+++ src/app/heros/heros.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service'
+@Component({
+  selector: 'app-heros',
+  templateUrl: './heros.component.html',
+  styleUrls: ['./heros.component.scss']
+})
+export class HerosComponent implements OnInit {
+  heroes: Hero[]
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+  getHeroes():void{
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes)
+  }
+  // getHeroes(): void {
+  //   this.heroService.getHeroes()
+  //       .subscribe(heroes => this.heroes = heroes);
+  // }
+}
